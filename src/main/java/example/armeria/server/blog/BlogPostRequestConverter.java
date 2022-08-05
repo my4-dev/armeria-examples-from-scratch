40478f582001bb8bde677a756310620117e1a0e4
my-is95 <miyoshi.exjge@gmail.com>
2022-08-05 23:44:42 +0900

    Implementing CREATE operation

---
+++ src/main/java/example/armeria/server/blog/BlogPostRequestConverter.java
@@ -0,0 +1,39 @@
+package example.armeria.server.blog;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.linecorp.armeria.common.AggregatedHttpRequest;
+import com.linecorp.armeria.server.ServiceRequestContext;
+import com.linecorp.armeria.server.annotation.RequestConverterFunction;
+
+import javax.annotation.Nullable;
+import java.lang.reflect.ParameterizedType;
+import java.util.concurrent.atomic.AtomicInteger;
+
+final class BlogPostRequestConverter implements RequestConverterFunction {
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private AtomicInteger idGenerator = new AtomicInteger();
+
+    static String stringValue(JsonNode jsonNode, String field) {
+        JsonNode value = jsonNode.get(field);
+        if (value == null) {
+            throw new IllegalArgumentException(field + " is missing!");
+        }
+        return value.textValue();
+    }
+
+    @Override
+    public Object convertRequest(ServiceRequestContext ctx,
+                                 AggregatedHttpRequest request, Class<?> expectedResultType,
+                                 @Nullable ParameterizedType expectedParameterizedResultType)
+        throws Exception {
+        if (expectedResultType == BlogPost.class) {
+            JsonNode jsonNode = mapper.readTree(request.contentUtf8());
+            int id = idGenerator.getAndIncrement();
+            String title = stringValue(jsonNode, "title");
+            String content = stringValue(jsonNode, "content");
+            return new BlogPost(id, title, content);    // Create an instance of BlogPost
+        }
+        return RequestConverterFunction.fallthrough();
+    }
+}
--- src/main/java/example/armeria/server/blog/BlogService.java
+++ src/main/java/example/armeria/server/blog/BlogService.java
@@ -1,8 +1,19 @@
 package example.armeria.server.blog;
 
+import com.linecorp.armeria.common.HttpResponse;
+import com.linecorp.armeria.server.annotation.Post;
+import com.linecorp.armeria.server.annotation.RequestConverter;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 public final class BlogService {
     private final Map<Integer, BlogPost> blogPosts = new ConcurrentHashMap<>();
+
+    @Post("/blogs")
+    @RequestConverter(BlogPostRequestConverter.class)
+    public HttpResponse createBlogPost(BlogPost blogPost) {
+        blogPosts.put(blogPost.getId(), blogPost);
+        return HttpResponse.ofJson(blogPost);
+    }
 }
